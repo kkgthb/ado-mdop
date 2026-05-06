27fdfc91ea5c6ccfe1c7ddb6715de800397b0c1f
kkgthb <44843658+kkgthb@users.noreply.github.com>
2026-05-06 13:57:09 -0500

    No, group alone does not work properly

--- .prereqs/AA-tf/modules/compute/main.tf
+++ .prereqs/AA-tf/modules/compute/main.tf
@@ -97,7 +97,100 @@ resource "azurerm_dev_center_project" "mydcp" {
 # # As expected before giving service principal ADO rights, Terraform apply errored out as precedes.
 # # I do not see "mdp-" in tfstate file, so yay, it seems to not go in corrupted, that's good.
 
-# Darnit.  ADO settings did not auto-delete when I deleted.  Otherwise seems promising though.
+# # Darnit.  ADO settings did not auto-delete when I deleted.  Otherwise seems promising though.
+# # Oh, and re-creating without first manually deleting the project-level and org-level fails like the below.
+# # I've got a suspicion that despite Microsoft's October 2025 announcement 
+# # (https://learn.microsoft.com/en-us/azure/devops/managed-devops-pools/features-timeline?view=azure-devops#october-2025)
+# # that the service principal no longer needs org-wide permissions, that they botched the implementation, 
+# # and that that actually is what's blocking proper destroy, or recreate-if-I-do-not-manually-destroy-ado-side-first.
+# # I think ADO mirrors the project-level one upon creation, then can't update it properly at the project level 
+# # once it's been mirrored to the org level unless the service principal has org access.  🤦‍♀️
+# # Update:  confirmed.  Gave my service principal org-wide agent pools access, and now it smoothly auto-deletes 
+# # the ADO side when I delete the Azure MDP, and also, before that, when I deleted the Azure side before I elevated 
+# # my service principal's privilege (thereby orphaning the ADO side) and recreated the Azure side after elevation, 
+# # that elevated service principal was able to "take over" the orphan and manage it just fine.
+# # So much for the October 2025 "improvement."  Sigh.
+# # But, good news is, the service principal doesn't require a paid ADO license.
+# ╷
+# │ Error: creating Pool (Subscription: "CENSORED"
+# │ Resource Group Name: "CENSORED-rg-demo"
+# │ Pool Name: "mdp-CENSORED"): polling after CreateOrUpdate: polling failed: the Azure API returned the following error:
+# │
+# │ Status: "Failed"
+# │ Code: "Failed"
+# │ Message: "The request has been completed with result Failed. Please check details with more information."
+# │ Activity Id: ""
+# │
+# │ ---
+# │
+# │ API Response:
+# │
+# │ ----[start]----
+# │ {
+# │     "id": "/subscriptions/CENSORED/providers/Microsoft.DevOpsInfrastructure/locations/CENTRALUS/operationStatuses/CENSORED*CENSORED",
+# │     "name": "CENSORED*CENSORED",
+# │     "resourceId": "/subscriptions/CENSORED/resourceGroups/CENSORED-rg-demo/providers/Microsoft.DevOpsInfrastructure/pools/mdp-CENSORED",
+# │     "status": "Failed",
+# │     "startTime": "CENSORED",
+# │     "endTime": "CENSORED",
+# │     "error": {
+# │         "message": "The request has been completed with result Failed. Please check details with more information.",
+# │         "details": [
+# │             {
+# │                 "code": "PoolProvisioningFailed",
+# │                 "message": "Failed to provision agent pool. Exception: Access denied. CENSORED_ENTRA_SP_NAME needs Manage permissions for pool mdp-CENSORED to perform the action. For more information, contact the Azure DevOps Server administrator.",
+# │                 "details": [],
+# │                 "additionalInfo": []
+# │             }
+# │         ],
+# │         "additionalInfo": []
+# │     }
+# │ }
+# │ -----[end]-----
+# │
+# │
+# │   with module.compute.azurerm_managed_devops_pool.managed_devops_pool,
+# │   on modules\compute\main.tf line 101, in resource "azurerm_managed_devops_pool" "managed_devops_pool":
+# │  101: resource "azurerm_managed_devops_pool" "managed_devops_pool" {
+# │
+# │ creating Pool (Subscription: "CENSORED"
+# │ Resource Group Name: "CENSORED-rg-demo"
+# │ Pool Name: "mdp-CENSORED"): polling after CreateOrUpdate: polling failed: the Azure API returned the following error:
+# │
+# │ Status: "Failed"
+# │ Code: "Failed"
+# │ Message: "The request has been completed with result Failed. Please check details with more information."
+# │ Activity Id: ""
+# │
+# │ ---
+# │
+# │ API Response:
+# │
+# │ ----[start]----
+# │ {
+# │     "id": "/subscriptions/CENSORED/providers/Microsoft.DevOpsInfrastructure/locations/CENTRALUS/operationStatuses/CENSORED*CENSORED",
+# │     "name": "CENSORED*CENSORED",
+# │     "resourceId": "/subscriptions/CENSORED/resourceGroups/CENSORED-rg-demo/providers/Microsoft.DevOpsInfrastructure/pools/mdp-CENSORED",
+# │     "status": "Failed",
+# │     "startTime": "CENSORED",
+# │     "endTime": "CENSORED",
+# │     "error": {
+# │         "message": "The request has been completed with result Failed. Please check details with more information.",
+# │         "details": [
+# │             {
+# │                 "code": "PoolProvisioningFailed",
+# │                 "message": "Failed to provision agent pool. Exception: Access denied. CENSORED_ENTRA_SP_NAME needs Manage permissions for pool mdp-CENSORED to perform the action. For more information, contact the Azure DevOps Server administrator.",
+# │                 "details": [],
+# │                 "additionalInfo": []
+# │             }
+# │         ],
+# │         "additionalInfo": []
+# │     }
+# │ }
+# │ -----[end]-----
+# │
+
+
 resource "azurerm_managed_devops_pool" "managed_devops_pool" {
   dev_center_project_id = azurerm_dev_center_project.mydcp.id
   name                  = "mdp-${var.workload_nickname}"
@@ -125,3 +218,94 @@ resource "azurerm_managed_devops_pool" "managed_devops_pool" {
     }
   }
 }
+
+# # Further notes
+# # 
+# # Remove existing user
+# # Are you sure you want to remove CENSORED_ENTRA_SP_NAME (CENSORED_CLIENT_ID) from the organization CENSORED?
+# # CENSORED_ENTRA_SP_NAME (CENSORED_CLIENT_ID) will continue to have access even after removal 
+# # if the user is a member of an Microsoft Entra group that has been added to this organization.
+# # 
+# # Cool, let's put that to the test ... hopefully my TF still works great since 
+# # I put CENSORED_ENTRA_SP_NAME into an Entra group that's part of my new 
+# # ADO "Project Collection Agent Pool Administrators" org-level group 
+# # and my new ADO "Project Agent Pool Administrators" project-level group.
+# #
+# # Result:  nope.  See error message:
+# ╷
+# │ Error: creating Pool (Subscription: "CENSORED"
+# │ Resource Group Name: "CENSORED-rg-demo"
+# │ Pool Name: "mdp-CENSORED"): polling after CreateOrUpdate: polling failed: the Azure API returned the following error:
+# │
+# │ Status: "Failed"
+# │ Code: "Failed"
+# │ Message: "The request has been completed with result Failed. Please check details with more information."
+# │ Activity Id: ""
+# │
+# │ ---
+# │
+# │ API Response:
+# │
+# │ ----[start]----
+# │ {
+# │     "id": "/subscriptions/CENSORED/providers/Microsoft.DevOpsInfrastructure/locations/CENTRALUS/operationStatuses/CENSORED*CENSORED",
+# │     "name": "CENSORED*CENSORED",
+# │     "resourceId": "/subscriptions/CENSORED/resourceGroups/CENSORED-rg-demo/providers/Microsoft.DevOpsInfrastructure/pools/mdp-CENSORED",
+# │     "status": "Failed",
+# │     "startTime": "CENSORED",
+# │     "endTime": "CENSORED",
+# │     "error": {
+# │         "message": "The request has been completed with result Failed. Please check details with more information.",
+# │         "details": [
+# │             {
+# │                 "code": "PoolProvisioningFailed",
+# │                 "message": "Failed to provision agent pool. Exception: The logged in user, CENSORED_ENTRA_OBJECT_ID, was not found in the Azure DevOps organization provided, https://dev.azure.com/CENSORED.",
+# │                 "details": [],
+# │                 "additionalInfo": []
+# │             }
+# │         ],
+# │         "additionalInfo": []
+# │     }
+# │ }
+# │ -----[end]-----
+# │
+# │
+# │   with module.compute.azurerm_managed_devops_pool.managed_devops_pool,
+# │   on modules\compute\main.tf line 194, in resource "azurerm_managed_devops_pool" "managed_devops_pool":
+# │  194: resource "azurerm_managed_devops_pool" "managed_devops_pool" {
+# │
+# │ creating Pool (Subscription: "CENSORED"
+# │ Resource Group Name: "CENSORED-rg-demo"
+# │ Pool Name: "mdp-CENSORED"): polling after CreateOrUpdate: polling failed: the Azure API returned the following error:
+# │
+# │ Status: "Failed"
+# │ Code: "Failed"
+# │ Message: "The request has been completed with result Failed. Please check details with more information."
+# │ Activity Id: ""
+# │
+# │ ---
+# │
+# │ API Response:
+# │
+# │ ----[start]----
+# │ {
+# │     "id": "/subscriptions/CENSORED/providers/Microsoft.DevOpsInfrastructure/locations/CENTRALUS/operationStatuses/CENSORED*CENSORED",
+# │     "name": "CENSORED*CENSORED",
+# │     "resourceId": "/subscriptions/CENSORED/resourceGroups/CENSORED-rg-demo/providers/Microsoft.DevOpsInfrastructure/pools/mdp-CENSORED",
+# │     "status": "Failed",
+# │     "startTime": "CENSORED",
+# │     "endTime": "CENSORED",
+# │     "error": {
+# │         "message": "The request has been completed with result Failed. Please check details with more information.",
+# │         "details": [
+# │             {
+# │                 "code": "PoolProvisioningFailed",
+# │                 "message": "Failed to provision agent pool. Exception: The logged in user, CENSORED_ENTRA_OBJECT_ID, was not found in the Azure DevOps organization provided, https://dev.azure.com/CENSORED.",
+# │                 "details": [],
+# │                 "additionalInfo": []
+# │             }
+# │         ],
+# │         "additionalInfo": []
+# │     }
+# │ }
+# │ -----[end]-----
